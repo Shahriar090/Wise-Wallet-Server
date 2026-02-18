db87a1018e015f8b5a83f3a8a07f358fe4b53701
Shahriar Hossain <shahriarh655@gmail.com>
2026-02-18 21:02:09 +0600

    chore(config): implemented a higher order function to prevent boilerplate try..catch logic within each single route

---
+++ src/app/utils/async_handler.ts
@@ -0,0 +1,9 @@
+import type { NextFunction, Request, RequestHandler, Response } from "express";
+
+export const asyncHandler = (func: RequestHandler) => {
+	return (req: Request, res: Response, next: NextFunction) => {
+		Promise.resolve(func(req, res, next)).catch((err) => {
+			next(err);
+		});
+	};
+};
