65b44e0f31e2f5c0af5b44583c88eb158a3e1019
Shahriar Hossain <shahriarh655@gmail.com>
2026-02-26 22:34:32 +0600

    feat(api): implemented a constant file for user module

---
+++ src/app/modules/user/user.constants.ts
@@ -0,0 +1,15 @@
+export const USER_ROLES = {
+	Admin: "Admin",
+	Member: "Member",
+} as const;
+
+export const USER_GENDER = {
+	Male: "Male",
+	Female: "Female",
+	Others: "Others",
+} as const;
+
+export const USER_STATUS = {
+	Active: "Active",
+	Blocked: "Blocked",
+} as const;
