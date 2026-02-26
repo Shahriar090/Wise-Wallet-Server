c02beef213e3c5a165d6cee89d23f007a44d06ec
Shahriar Hossain <shahriarh655@gmail.com>
2026-02-27 00:05:31 +0600

    feat(api): implemented service logic to update a user info

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -19,6 +19,60 @@ const createUserIntoDb = async (payload: UserType) => {
 	return await User.create(payload);
 };
 
+// update user info
+const updateUserIntoDb = async (userId: string, payload: UserType) => {
+	// separating primitive and non primitive data
+	const { name, contactInfo, preferences, ...remainingData } = payload;
+
+	const modifiedUpdatedData: Record<string, unknown> = {
+		...remainingData,
+	};
+
+	// updating name object
+	if (name && typeof name === "object" && Object.keys(name).length) {
+		for (const [key, value] of Object.entries(name)) {
+			modifiedUpdatedData[`name.${key}`] = value;
+		}
+	}
+
+	// updating contact info object
+	if (
+		contactInfo &&
+		typeof contactInfo === "object" &&
+		Object.keys(contactInfo).length
+	) {
+		for (const [key, value] of Object.entries(contactInfo)) {
+			modifiedUpdatedData[`contactInfo.${key}`] = value;
+		}
+	}
+
+	// updating preferences
+	if (
+		preferences &&
+		typeof preferences === "object" &&
+		Object.keys(preferences).length
+	) {
+		for (const [key, value] of Object.entries(preferences)) {
+			modifiedUpdatedData[`preferences.${key}`] = value;
+		}
+	}
+
+	const result = await User.findByIdAndUpdate(userId, modifiedUpdatedData, {
+		new: true,
+	});
+
+	if (!result) {
+		throw new AppError(
+			httpStatus.NOT_FOUND,
+			"User Not Found",
+			"UserNotFoundError",
+		);
+	}
+
+	return result;
+};
+
 export const UserServices = {
 	createUserIntoDb,
+	updateUserIntoDb,
 };
